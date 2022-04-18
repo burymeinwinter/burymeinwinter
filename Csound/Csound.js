e4adca099e9d5aeabbf8b7e594b5e4333f76e032
burymeinwinter <84890976+burymeinwinter@users.noreply.github.com>
2022-04-17 23:39:22 -0600

    Update Csound.js

--- Csound/Csound.js
+++ Csound/Csound.js
@@ -1,3 +1,30 @@
+
+#https://cljdoc.org/d/csound-wasm/csound-wasm/6.11.0-1/doc/readme
+#https://github.com/hlolli/csound-wasm/releases
+
+
+
+
+const csound = require('csound-wasm');
+
+const beeper = `
+instr 1
+  asig = poscil:a(0.3, 440)
+  outc asig, asig
+endin`
+
+const makeBeep = `i 1 0 1`
+
+csound.startRealtime()
+csound.compileOrc(beeper);
+csound.readScore(makeBeep);
+
+setTimeout(() => process.exit(), 5000);
+
+
+//////
+
+
 var get_csound = async function(csound_message_callback_) {
     if (csound_is_loaded === false) {
         await load_csound(csound_message_callback_);
